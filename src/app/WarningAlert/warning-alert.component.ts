6695a265048d1b94b5c2b71992733c2ddb9466a8
Daniel Straus <strausd@me.com>
2017-05-16 13:34:29 -0500

    Complete first assignment with two basic components

---
+++ src/app/WarningAlert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-warningAlert',
+    templateUrl: './WarningAlert.component.html'
+})
+export class WarningAlertComponent {
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-warningAlert',
+    templateUrl: './warning-alert.component.html'
+})
+export class WarningAlertComponent {
+
+}
